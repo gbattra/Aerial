69dac9031dd9996ecb1fc191a9ff1201c918347c
Gregory Broderick Attra <greg.attra@gmail.com>
2020-05-16 19:47:42 -0400

    Created minigun

---
+++ Assets/Scripts/Abilities/Minigun.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Minigun : MonoBehaviour
+{
+    public Vehicle vehicle;
+    public ParticleSystem ProjectilePs;
+    public ParticleSystem MuzzleFlashPs;
+    public ParticleSystem SleevesPs;
+
+    public float FireRate;
+
+    public AudioClip AudioClip;
+    public AudioSource AudioSource;
+
+    private float _time;
+
+    private void FixedUpdate()
+    {
+		
+        if (Math.Abs(Controller.rightTrigger) < 1f)
+            return;
+		
+        _time += Time.deltaTime;
+		
+        if (_time < FireRate || vehicle.shieldAbility.isShielding)
+            return;
+
+        ProjectilePs.Emit(1);
+        MuzzleFlashPs.Play(true);
+        SleevesPs.Emit(1);
+
+        if (AudioSource != null && AudioClip != null)
+            AudioSource.PlayOneShot(AudioClip);
+
+        _time = 0;
+    }
+}
