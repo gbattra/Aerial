b18e030ab574bfc0b288aca286b52d98dd2d8b94
Gregory Broderick Attra <greg.attra@gmail.com>
2020-05-17 18:43:06 -0400

    started on hud script

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DuloGames.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Player player;
+    public UIBulletBar healthRadialBar;
+    public UIProgressBar minigunCharge;
+    public UIProgressBar boostCharge;
+    
+    public Text healthRadialBarText;
+    public Text minigunChargeText;
+    public Text boostChargeText;
+
+    public void Start()
+    {
+        healthRadialBar.fillAmount = 1f;
+        healthRadialBarText.text = "100%";
+
+        minigunCharge.fillAmount = 1f;
+        minigunChargeText.text = "100%";
+
+        boostCharge.fillAmount = 1f;
+        boostChargeText.text = "100%";
+    }
+}
