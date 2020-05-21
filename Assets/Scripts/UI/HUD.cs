e528ba04d94ebaefa7aaeec5e1f8415eb9abb2ae
Gregory Broderick Attra <greg.attra@gmail.com>
2020-05-20 20:30:40 -0400

    resized level clear / countdown text

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -32,6 +32,7 @@ public class HUD : MonoBehaviour
 
     private int currentLevelNumber;
     private float timeLevelClearAnnounced;
+    private bool announcingLevelClear;
 
     public void Start()
     {
@@ -96,16 +97,24 @@ public class HUD : MonoBehaviour
         {
             levelProgressRadial.UpdateStatus(
                 levelManager.countdownSecondsRemaining, levelManager.secondsAtMax);
-        }
-
-        if (levelManager.levelNumber > currentLevelNumber)
+            if (levelManager.countdownSecondsRemaining < 3)
+                levelAnnouncement.text = "3";
+            if (levelManager.countdownSecondsRemaining < 2)
+                levelAnnouncement.text = "2";
+            if (levelManager.countdownSecondsRemaining < 1)
+                levelAnnouncement.text = "1";
+        } else if (levelManager.levelNumber > currentLevelNumber)
         {
+            announcingLevelClear = true;
             currentLevelNumber++;
             levelAnnouncement.text = "LEVEL CLEAR";
             timeLevelClearAnnounced = Time.time;
         }
 
-        if (Time.time - timeLevelClearAnnounced > 3f)
+        if (announcingLevelClear && Time.time - timeLevelClearAnnounced > 3f)
+        {
+            announcingLevelClear = false;
             levelAnnouncement.text = "";
+        }
     }
 }
